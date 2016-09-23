d33261b40c9bdd371c145d2721d54cd84de38ab6
Adam Israel <adam@adamisrael.com>
2016-09-23 16:35:33 +0200

    Make scripts more idempotent

--- scripts/create-datacenter.sh
+++ scripts/create-datacenter.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
-
+HOME=/home/openmanod
+OPENMANO=$HOME/bin/openmano
 export OPENMANO_TENANT=$4
-export OPENMANO_DATACENTER=`/home/openmanod/bin/openmano datacenter-create myov http://$1:$2/openvim |gawk '{print $1}'`
-#echo "export OPENMANO_DATACENTER=$OPENMANO_DATACENTER " >> /home/openmanod/.bashrc
-/home/openmanod/bin/openmano datacenter-attach myov --vim-tenant-name $3
+
+OPENMANO_DATACENTER=`$OPENMANO datacenter-list myov`
+if [ $? -ne 0 ]; then
+    OPENMANO_DATACENTER=`$OPENMANO datacenter-create myov http://$1:$2/openvim`
+fi
+export OPENMANO_DATACENTER=`echo $OPENMANO_DATACENTER |gawk '{print $1}'`
+
+#export OPENMANO_DATACENTER=`$OPENMANO datacenter-create myov http://$1:$2/openvim |gawk '{print $1}'`
+# FIXME: don't add this to .bashrc if it already exists.
+if ! grep -q "^export OPENMANO_DATACENTER" $HOME/.bashrc
+then
+    echo "export OPENMANO_DATACENTER=$OPENMANO_DATACENTER " >> $HOME.bashrc
+fi
+
+# TODO: Test idempotency. We may need to check and remove existing data
+$OPENMANO datacenter-attach myov --vim-tenant-name $3
+$OPENMANO datacenter-netmap-import -f --datacenter $OPENMANO_DATACENTER
--- scripts/create-tenant.sh
+++ scripts/create-tenant.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-OPENMANO_TENANT=`/home/openmanod/bin/openmano tenant-create mytenant --description=mytenant |gawk '{print $1}'`
-echo $OPENMANO_TENANT
+OPENMANO=/home/openmanod/bin/openmano
+OPENMANO_TENANT=`$OPENMANO tenant-create mytenant --description=mytenant`
+if [ $? -ne 0 ]; then
+    OPENMANO_TENANT=`$OPENMANO tenant-list mytenant`
+fi
+echo $OPENMANO_TENANT |gawk '{print $1}'
